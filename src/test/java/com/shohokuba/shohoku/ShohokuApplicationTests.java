d392f9eb68b9a777d2dbe8768fe5d3db650756f8
Oscarmageddon <oscarmageddon@gmail.com>
2022-01-12 15:58:47 -0300

    first commit

---
+++ src/test/java/com/shohokuba/shohoku/ShohokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shohokuba.shohoku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShohokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
